ef8363217a05acef9aed8aea974ac2460cb389ea
tpcm <peadermartin@gmail.com>
2024-08-22 06:36:22 +0000

    update main event loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 import pygame
 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from bullets import Shot
 
 from constants import (
     SCREEN_WIDTH,
@@ -19,20 +23,42 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, updatable, drawable)
 
     Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        for to_update_obj in updatable:
+            to_update_obj.update(dt=dt)
+
+        
+
+        for asteroid in asteroids:
+            if asteroid.detect_collision(player):
+                print("Game over!")
+                sys.exit()
+            
+            for shot in shots:
+                if asteroid.detect_collision(shot):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill(color="black")
 
-        for to_update, to_draw in zip(updatable, drawable):
-            to_update.update(dt=dt)
-            to_draw.draw(screen=screen)
+        for to_draw_obj in drawable:
+            to_draw_obj.draw(screen=screen)
 
         pygame.display.flip()
 
